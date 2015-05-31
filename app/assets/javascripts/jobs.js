73d83c7ed493510d08cf9d3076d7c42b62e516fa
Hon Lam <lamh85@gmail.com>
2015-05-31 15:33:13 -0700

    Started rendering JSON data using Angular

--- app/assets/javascripts/jobs.js
+++ app/assets/javascripts/jobs.js
@@ -1,9 +1,16 @@
-var app = angular.module("JobsIndex", []);
+var app = angular.module("Barterist", []);
 
-app.controller("JobsController", ['$http', function($http) 
+app.controller("JobsController", ['$http', function($http) {
+
+  var jobsList = this;
+  jobsList.jobs = [];
+
   $http.get('/jobs.json').success(function(data){
-
-  }) // Success function
+    jobsList.jobs = data;
+  }). // Success function
+  error(function(data){
+    console.log("Could not retreive JSON data!");
+  });
 
 }]); // Controller
 
