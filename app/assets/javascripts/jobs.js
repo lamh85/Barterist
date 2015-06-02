9a7159fbd2e62899bd8f9910f733a83e822f198b
Hon Lam <lamh85@gmail.com>
2015-06-02 16:45:48 -0700

    Experimented with scopes

--- app/assets/javascripts/jobs.js
+++ app/assets/javascripts/jobs.js
@@ -1,40 +1,48 @@
-var app = angular.module("Barterist", []);
+var jobsGlobal = [];
 
-app.controller("JobsController", ['$http', function($http) {
+(function(){
+  var app = angular.module("Barterist", []);
 
-  var jobsList = this;
-  jobsList.jobs = [];
+  app.controller("JobsController", ['$http', function($http) {
 
-  $http.get('/jobs.json').success(function(data){
-    jobsList.jobs = data;
-  }). // Success function
-  error(function(data){
-    console.log("Could not retreive JSON data!");
+    var jobsList = this;
+    jobsList.jobs = [];
+
+    $http.get('/jobs.json').success(function(data){
+      jobsList.jobs = data;
+      jobsGlobal = data;
+    }). // Success function
+    error(function(data){
+      console.log("Could not retreive JSON data!");
+    });
+
+    console.log("jobsGlobal = " + jobsGlobal);
+
+    // The default job selected
+    this.selectedJob = 0;
+
+    // Find the job of above index
+    this.selectedJobObject = this.jobs[this.selectedJob];
+
+    // Select the job via index number from the array
+    this.selectJob = function(jobNumber) {
+      this.selectedJob = jobNumber;
+    };
+
+    // A tester that checks if the inputted number is the currently selected job
+    this.isSelected = function(jobNumber) {
+      return this.selectJob === jobNumber;
+    };
+
+    var fromJobs = "hello from Jobs Controller";
+
+  }]); // Controller
+
+  app.controller("ReceiverController", function() {
+
+    this.checkpoint = "hello world";
+    // var receiverVar = JobsController.fromJobs;
+
   });
 
-  // The default job selected
-  this.selectedJob = 0;
-
-  this.testObject = this.jobs[this.selectedJob];
-
-  // Find the job of above index
-  this.selectedJobObject = this.jobs[this.selectedJob];
-
-  // Select the job via index number from the array
-  this.selectJob = function(jobNumber) {
-    this.selectedJob = jobNumber;
-  };
-
-  // A tester that checks if the inputted number is the currently selected job
-  this.isSelected = function(jobNumber) {
-    return this.selectJob === jobNumber;
-  };
-
-}]); // Controller
-
-
-// $(document).ready(function() {
-//   $('#container').html(app.JobsController);
-// });
-
-var bar = "hello world";
+})();
