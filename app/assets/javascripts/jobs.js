9e4fc0e8182eb4c8bda66984f8c2b3969cacea5e
Hon Lam <lamh85@gmail.com>
2015-06-02 15:08:38 -0700

    Bound the jobs list to display panel

--- app/assets/javascripts/jobs.js
+++ app/assets/javascripts/jobs.js
@@ -12,10 +12,29 @@ app.controller("JobsController", ['$http', function($http) {
     console.log("Could not retreive JSON data!");
   });
 
+  // The default job selected
+  this.selectedJob = 0;
+
+  this.testObject = this.jobs[this.selectedJob];
+
+  // Find the job of above index
+  this.selectedJobObject = this.jobs[this.selectedJob];
+
+  // Select the job via index number from the array
+  this.selectJob = function(jobNumber) {
+    this.selectedJob = jobNumber;
+  };
+
+  // A tester that checks if the inputted number is the currently selected job
+  this.isSelected = function(jobNumber) {
+    return this.selectJob === jobNumber;
+  };
+
 }]); // Controller
+
 
 // $(document).ready(function() {
 //   $('#container').html(app.JobsController);
 // });
 
-var foo = "hello world";
+var bar = "hello world";
