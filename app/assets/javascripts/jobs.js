0465ac801b82f33e68b465a411eb40e9f53ba42e
Hon Lam <lamh85@gmail.com>
2015-06-10 21:26:15 -0700

    Can now AJAX render new comments after posting.

--- app/assets/javascripts/jobs.js
+++ app/assets/javascripts/jobs.js
@@ -17,36 +17,24 @@ var JobsController;
 
   JobsController = app.controller("JobsController", ['$http','CommonFunctions','$scope', function($http,CommonFunctions,$scope) {
 
-    // Store the value of "this" into a variable so it can be used in the $http.get function
+    // Store the value of "this" into a variable so it can be used in the $http.get function, AND in the $http.post function
     var thisVar = this;
 
     thisVar.jobs = [];
     // This variable will contain the job selected via this.selectJob function
     thisVar.selectedJobObject = {};
+    this.selectedJobIndex;
 
+    // Set the content for the RIGHT side of the webpage
     this.selectJob = function(jobIndexInput) {
-      jobIndex = jobIndexInput;
+      console.log("The selected job's index is: " +jobIndexInput);
+      this.selectedJobIndex = jobIndexInput;
       thisVar.selectedJobObject = thisVar.jobs[jobIndexInput];
-      $('#comment_user_id').attr('value', thisVar.selectedJobObject.id);
+      $('#comment_user_id').attr('value', jobIndexInput);
     };
 
-    this.postData = function() {
-      console.log("function fired!");
-      // this.jobs = [];
-      // this.selectedJobObject = {};
-
-      var postDataObject = {
-        title: $('#comment_title').val(),
-        body: $('#comment_body').val(),
-        user_id: $('#comment_user_id').val()
-      }
-
-      $http.post('/comments', postDataObject).success(function(){
-        console.log("Successfully posted");
-      });
-    }
-
-    JobsCtrlGetJson = function(jobIndexInput){
+    // GET the JSON data
+    this.getJson = function(jobIndexInput){
       // Select the job via index number from the array
       $http.get('/jobs.json').success(function(data){
         dataGlobal = thisVar.jobs = data;
@@ -57,8 +45,26 @@ var JobsController;
         console.log("Could not retreive JSON data!");
       });
     }
+    this.getJson(0);
 
-    JobsCtrlGetJson(0);
+    // AJAX POST call
+    this.postComment = function() {
+      console.log("function fired!");
+      // this.jobs = [];
+      // this.selectedJobObject = {};
+
+      var postDataObject = {
+        title: $('#comment_title').val(),
+        body: $('#comment_body').val(),
+        job_id: this.selectedJobObject.id
+      }
+
+      $http.post('/comments', postDataObject).success(function(){
+        console.log("Successfully posted");
+        // Refresh the JSON data
+        thisVar.getJson(thisVar.selectedJobIndex);
+      });
+    } // AJAX POST call
 
     // A tester that checks if the inputted number is the currently selected job
     this.isSelected = function(jobIndexInput) {
@@ -71,24 +77,3 @@ var JobsController;
   }]); // Controller
 
 })();
-
-$('#new_comment');
-$('#comment_title').val();
-$('#comment_body').val();
-$('#new_comment .button-submit');
-
-// Form modifications - Under construction
-// $('.new_comment .button-submit') // Button for submitting new comment
-// $('.new_comment').attr('action',''); // Stripping the action attribute
-
-// Example of jQuery post
-// $("#timer-button").on("click", function() {
-//  $.ajax({
-//    method: "POST",
-//    url: "/clock_timers",
-//    data: {clock_timer: {time: timeSoFar}},
-//    error: function() {alert("error!")},
-//    success: function(response) {
-//      console.log(response);
-//    }
-// })
